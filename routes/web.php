ee1ce29b10c41b0eb200529c967f1ed1fe53dba0
jmusila <jonathanmusila6@gmail.com>
2021-04-30 11:36:13 +0300

    correct the routes

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,3 @@ Route::prefix('countries')->group(function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
